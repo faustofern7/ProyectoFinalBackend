53e3462e2bff00f68c5a7a13183ec1e144b2aa62
Fausto Fernandez <fausto44190084@gmail.com>
2024-08-11 21:48:17 -0300

    desarrolle el archivo usuarios routes

---
+++ src/routes/usuarios.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { crearUsuario, login } from "../controllers/usuarios.controllers.js";
+
+const router = Router();
+
+router.route('/registrar').post(crearUsuario);
+router.route('/').post(login);
+
+export default router;
