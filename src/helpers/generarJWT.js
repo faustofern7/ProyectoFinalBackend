004b3d4d1f1baaec61ac6e6a84024e4b83c6b870
Fausto Fernandez <fausto44190084@gmail.com>
2024-08-11 21:43:09 -0300

    desarrolle el archivo generar JWT

---
+++ src/helpers/generarJWT.js
@@ -0,0 +1,15 @@
+import jwt  from "jsonwebtoken";
+
+const generarJWT = async (uid, email)=>{
+    try {
+        const payload = { uid, email };
+        const token = await jwt.sign(payload, process.env.SECRET_JWT, {
+            expiresIn: '3h' 
+        });
+        return token;
+    } catch (error) {
+        console.error('Error al generar el token:', error.message);
+        throw new Error('No se pudo generar el token');
+    }
+}
+export default generarJWT
